d52033c1f7576006dc9a63c7c84c1696caa29457
SaykatMukherjee <muksay34@yahoo.in>
2022-08-17 21:47:39 +0530

    Azure Deployment

---
+++ build/precache-manifest.f44c17961effe01e5b7681130a97ac7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e73e5be3de1d1b377072015dd5dd740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07069a4344a10baae9a6",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "0eb1ed69df63190dcbca",
+    "url": "/static/js/2.3e22c410.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3e22c410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07069a4344a10baae9a6",
+    "url": "/static/js/main.2e575d88.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2e575d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5393f577450d10213e1",
+    "url": "/static/js/runtime-main.2b57d9df.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
