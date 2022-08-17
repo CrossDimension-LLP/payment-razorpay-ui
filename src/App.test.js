62a7b22ca4e5319e13e4f806df58d1d628f93adf
SaykatMukherjee <muksay34@yahoo.in>
2022-08-17 23:01:04 +0530

    removed test file

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { getByTestId, render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const linkElement = getByTestId(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
