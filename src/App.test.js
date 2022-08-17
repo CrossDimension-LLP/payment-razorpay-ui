241a962118fa84b419c3f903dcb3ef5c9063ad4d
SaykatMukherjee <muksay34@yahoo.in>
2022-08-17 22:18:30 +0530

    test class modified

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { getByTestId, render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByTestId(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
