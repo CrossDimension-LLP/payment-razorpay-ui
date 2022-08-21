a444853e98f50d8fe0ae443d5a4bbb99fa74ebbb
Poonam Dubey <poonamdubey1294@gmail.com>
2022-08-21 17:17:21 +0530

    remove unused ui

--- src/App.js
+++ src/App.js
@@ -101,20 +101,7 @@ const App = () => {
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<button
-					className="App-link"
-					onClick={displayRazorpay}
-					rel="noopener noreferrer"
-					
-				>
-					Donate $5
-				</button>
-			</header>
+			{displayRazorpay()}
 		</div>
 	)
 }
